35da149ca9e8d494727cea07d5ae1cde6ad37bf2
soenneker <jake@soenneker.com>
2026-08-29 12:38:55 -0500

    Improve XML documentation

--- src/Soenneker.Extensions.Bool/BoolExtension.cs
+++ src/Soenneker.Extensions.Bool/BoolExtension.cs
@@ -7,14 +7,22 @@ namespace Soenneker.Extensions.Bool;
 /// </summary>
 public static class BoolExtension
 {
-    /// <returns>"Yes" or "No"</returns>
+    /// <summary>
+    /// Converts the Boolean value to the display text <c>Yes</c> or <c>No</c>.
+    /// </summary>
+    /// <param name="value">The Boolean value to format.</param>
+    /// <returns><c>Yes</c> for true; otherwise <c>No</c>.</returns>
     [Pure]
     public static string ToYesNo(this bool value)
     {
         return value ? "Yes" : "No";
     }
 
-    /// <returns>"yes" or "no"</returns>
+    /// <summary>
+    /// Converts the Boolean value to the lowercase text <c>yes</c> or <c>no</c>.
+    /// </summary>
+    /// <param name="value">The Boolean value to format.</param>
+    /// <returns><c>yes</c> for true; otherwise <c>no</c>.</returns>
     [Pure]
     public static string ToYesNoLower(this bool value)
     {
